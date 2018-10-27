794339e24b45bd74808d732a4e078e153211d868
Dan McKeon <dpmckeon@gmail.com>
2018-10-26 20:25:10 -0700

    get rid of css and testing (for now), will use jss

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,6 +1,4 @@
 import * as React from 'react';
-import './App.css';
-
 import logo from './logo.svg';
 
 class App extends React.Component {
@@ -9,11 +7,9 @@ class App extends React.Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">Hello React World</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.tsx</code> and save to reload.
-        </p>
+        <p className="App-intro">Here's some content</p>
       </div>
     );
   }
