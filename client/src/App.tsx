c1a9805de62069c34ff6d69fbda096230ae85ea5
Dan McKeon <dpmckeon@gmail.com>
2018-10-26 21:23:07 -0700

    react-jss not working, but next task will be no 2 package.json, configs, etc

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,25 +2,54 @@ import * as React from 'react';
 import injectSheet from 'react-jss';
 import logo from './logo.svg';
 
+// .App-logo {
+//   animation: App-logo-spin infinite 20s linear;
+//   height: 40vmin;
+// }
+
+// .App-header {
+//   background-color: #282c34;
+//   min-height: 100vh;
+//   display: flex;
+//   flex-direction: column;
+//   align-items: center;
+//   justify-content: center;
+//   font-size: calc(10px + 2vmin);
+//   color: white;
+// }
+
+// .App-link {
+//   color: #61dafb;
+// }
+
 const styles = {
-  container: {
-    marginBottom: 60
+  app: {
+    textAlign: 'center'
+  },
+  appIntro: {
+    color: 'red'
+  },
+  appTitle: {
+    color: 'yellow'
   }
 };
 
+interface IProps {
+  classes?: any;
+}
+
 @injectSheet(styles)
-class App extends React.Component {
+export default class App extends React.Component<IProps, {}> {
   public render() {
+    const { classes } = this.props;
     return (
-      <div className="App">
-        <header className="App-header">
+      <div className="app">
+        <header className="appHeader">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Hello React World2</h1>
+          <h1 className={classes.appTitle}>Hello React World3</h1>
         </header>
-        <p className="App-intro">Here's some content</p>
+        <p className={classes.appIntro}>Here's some content</p>
       </div>
     );
   }
 }
-
-export default App;
