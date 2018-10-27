e4b41d2f16c188a35f4edb7b356e5335019a56d7
Dan McKeon <dpmckeon@gmail.com>
2018-10-26 20:26:46 -0700

    new webpack config to handle react

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 const path = require('path');
 const slsw = require('serverless-webpack');
 const nodeExternals = require('webpack-node-externals');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 module.exports = {
   entry: slsw.lib.entries,
@@ -14,10 +15,33 @@ module.exports = {
     rules: [
       {
         test: /\.tsx?$/,
+        include: __dirname, //do we want this?
         exclude: /node_modules/,
+        loader: 'ts-loader'
+        // query: {
+        //   presets: ['es2015', 'react-app', 'stage-2'],
+        //   plugins: ['css-modules-transform']
+        // }
+      },
+      {
+        test: /\.css?$/,
+        include: __dirname, //do we want this?
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+        query: {
+          presets: ['es2015', 'react-app', 'stage-2'],
+          plugins: ['css-modules-transform']
+        }
+      },
+      {
+        test: /\.(png|jp(e*)g|svg)$/,
         use: [
           {
-            loader: 'ts-loader'
+            loader: 'url-loader',
+            options: {
+              limit: 8000,
+              name: 'images/[hash]-[name].[ext]'
+            }
           }
         ]
       }
@@ -26,10 +50,15 @@ module.exports = {
   resolve: {
     extensions: ['.tsx', '.ts', '.js']
   },
-  output: {
-    libraryTarget: 'commonjs2',
-    path: path.join(__dirname, '.webpack'),
-    filename: '[name].js',
-    sourceMapFilename: '[file].map'
-  }
+  plugins: [
+    new CopyWebpackPlugin([{ from: 'client/build', to: 'build' }], {
+      debug: 'info'
+    })
+  ]
+  // output: {
+  //   libraryTarget: 'commonjs2',
+  //   path: path.join(__dirname, '.webpack'),
+  //   filename: '[name].js',
+  //   sourceMapFilename: '[file].map'
+  // }
 };
