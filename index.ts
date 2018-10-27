a07ba1e3a669621bee3de97cffbb7cb1f27d0a58
Dan McKeon <dpmckeon@gmail.com>
2018-10-26 20:00:46 -0700

    update index

--- index.ts
+++ index.ts
@@ -2,7 +2,12 @@
 const express = require('express');
 const serverless = require('serverless-http');
 const app = express();
-app.get('/', function(req, res) {
-  res.send('Hello New World!');
-});
+const path = require('path');
+import renderer from './middleware/renderer';
+
+// root (/) should always serve our server rendered page
+app.use('^/$', renderer);
+// serve static assets
+app.use(express.static(path.join(__dirname, 'client', './build')));
+// handler
 export const handler = serverless(app);
