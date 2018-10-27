3f07b77f23dfeb18164c12b5d6faa2a1e321ef21
Dan McKeon <dpmckeon@gmail.com>
2018-10-26 19:31:52 -0700

    a bit of house cleaning

--- webpack.config.js
+++ webpack.config.js
@@ -1,19 +1,13 @@
-// webpack.config.js
 const path = require('path');
 const slsw = require('serverless-webpack');
 const nodeExternals = require('webpack-node-externals');
+
 module.exports = {
   entry: slsw.lib.entries,
   target: 'node',
   mode: slsw.lib.webpack.isLocal ? 'development' : 'production',
-  optimization: {
-    // We no not want to minimize our code.
-    minimize: false
-  },
-  performance: {
-    // Turn off size warnings for entry points
-    hints: false
-  },
+  optimization: { minimize: false },
+  performance: { hints: false },
   devtool: 'nosources-source-map',
   externals: [nodeExternals()],
   module: {
